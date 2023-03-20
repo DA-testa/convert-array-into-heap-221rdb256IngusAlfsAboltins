c0854b26dd0381c06c38bfc263b546afe4037126
221rdb256IngusAlfsAboltins <128421546+221rdb256IngusAlfsAboltins@users.noreply.github.com>
2023-03-21 00:50:56 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,50 +1,46 @@
 def heapify(data, i, n, swaps):
-    min_idx = i
-    left_child_idx = 2 * i + 1
-    right_child_idx = 2 * i + 2
+    min_index = i
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
 
-    if left_child_idx < n and data[left_child_idx] < data[min_idx]:
-        min_idx = left_child_idx
-
-    if right_child_idx < n and data[right_child_idx] < data[min_idx]:
-        min_idx = right_child_idx
-
-    if i != min_idx:
-        swaps.append((i, min_idx))
-        data[i], data[min_idx] = data[min_idx], data[i]
-        heapify(data, min_idx, n, swaps)
-
-
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        heapify(data, min_index, n, swaps)
 def build_heap(data):
     n = len(data)
     swaps = []
     for i in range(n // 2, -1, -1):
         heapify(data, i, n, swaps)
     return swaps
-
-
 def main():
-    text = input().strip()
-    if text == "I":
-        n = int(input().strip())
-        data = list(map(int, input().strip().split()))
+    first_input = input().strip()
+    if first_input == 'I':
+        
+        
+        
+        n = int(input())
+        data = list(map(int, input().split()))
         assert len(data) == n
-        swaps = build_heap(data)
-        print(len(swaps))
-        for i, j in swaps:
-            print(i, j)
-     elif input_type == "F":
+    elif first_input == 'F':
         file_name = input().strip()
-        with open(file_name, 'r') as f:
+        file_path = os.path.join('tests', file_name)
+        with open(file_path, 'r') as f:
             n = int(f.readline())
             data = list(map(int, f.readline().split()))
             assert len(data) == n
-            swaps = build_heap(data)
-            print(len(swaps))
-            for i, j in swaps:
-                print(i, j)
-    
-    
-    
+    else:
+        return
+    swaps = build_heap(data)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
+        
+        
+        
 if __name__ == "__main__":
     main()
