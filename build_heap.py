95f3718f0d62214e6165795973a4ef28e968919e
221rdb256IngusAlfsAboltins <128421546+221rdb256IngusAlfsAboltins@users.noreply.github.com>
2023-03-20 22:41:55 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,33 @@
-# python3
+def heapify(data, i, n, swaps):
+    min_idx = i
+    left_child_idx = 2 * i + 1
+    right_child_idx = 2 * i + 2
+
+    if left_child_idx < n and data[left_child_idx] < data[min_idx]:
+        min_idx = left_child_idx
+
+    if right_child_idx < n and data[right_child_idx] < data[min_idx]:
+        min_idx = right_child_idx
+
+    if i != min_idx:
+        swaps.append((i, min_idx))
+        data[i], data[min_idx] = data[min_idx], data[i]
+        heapify(data, min_idx, n, swaps)
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(n // 2, -1, -1):
+        heapify(data, i, n, swaps)
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
@@ -40,3 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
